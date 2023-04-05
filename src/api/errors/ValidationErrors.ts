627a328ca3181c1fcde3debc5ed1396b9f2b7546
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-04-05 18:50:19 +0530

    Tmp, replace once zod

---
+++ src/api/errors/ValidationErrors.ts
@@ -0,0 +1,7 @@
+class InvalidUsername extends Error {}
+
+class IntegrityFailure extends Error {}
+
+class InvalidCredential extends Error {}
+
+export { IntegrityFailure, InvalidUsername, InvalidCredential };
