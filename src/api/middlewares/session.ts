4dae46d14d25513743064985c21c0daae50f234f
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-06-19 19:55:42 +0530

    Auth header should be like `Bearer token`

--- src/api/middlewares/session.ts
+++ src/api/middlewares/session.ts
@@ -3,7 +3,7 @@ import type { NextFunction, Request, Response } from 'express';
 import { HttpCodes } from '../../Constants';
 
 const attachSession = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  const jwtToken = req.headers.authorization;
+  const jwtToken = req.headers.authorization?.replace('Bearer ', '');
   try {
     if (jwtToken)
       req.payload = await getJwtPayload(jwtToken, true);
