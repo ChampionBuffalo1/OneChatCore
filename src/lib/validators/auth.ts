e5fb31f34b39a8d45cf7efcdbf9e3d5846e77cc6
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-10-13 20:38:10 +0530

    nonempty() is deprecated

--- src/lib/validators/auth.ts
+++ src/lib/validators/auth.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 const authBody = z.object({
-  username: z.string().nonempty(),
-  password: z.string().nonempty()
+  username: z.string().min(1),
+  password: z.string().min(1)
 });
 
 type authBody = z.infer<typeof authBody>;
