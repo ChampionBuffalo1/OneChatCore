d5b995d6bef1b7e8af1c38352e3624c76954d090
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-10-13 20:41:50 +0530

    Removing redis lazy Connection
    Due to lazy connection the signing of
    first jwt token after startup causes
    the request to fail.

--- src/lib/createConnection.ts
+++ src/lib/createConnection.ts
@@ -7,7 +7,6 @@ const prisma = new PrismaClient();
 prisma.$connect().then(() => Logger.info('Prisma client connected.'));
 
 let redis: Redis | undefined = new Redis(redisURL, {
-  lazyConnect: true,
   retryStrategy: (times: number) => {
     if (times > maxRetries) throw new Error('Maximum retry limit reached for redis.');
     return defaultDelay;
