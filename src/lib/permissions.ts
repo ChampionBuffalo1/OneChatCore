842eefa7638a6a9129069a53fff66ba478da5bc6
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-02-01 00:17:03 +0530

    Added some more utilities

--- src/lib/permissions.ts
+++ src/lib/permissions.ts
@@ -11,21 +11,60 @@ export const Permissions = {
   INVITE_MEMBER: 1 << 5,
   KICK_MEMBER: 1 << 6,
   MANAGE_GROUP: 1 << 7, // modify group name, desc etc
-  ADMININSTRATOR: (1 << 7) - 1 // 111 11 111
+  ADMINISTRATOR: (1 << 7) - 1 // 111 11 111
+};
+
+type AllPermissions = keyof typeof Permissions;
+type AllowedPermission = AllPermissions | AllPermissions[];
+type PermissionRecord = {
+  denied: AllPermissions[];
+  allowed: AllPermissions[];
 };
 
 // Reference: https://en.wikipedia.org/wiki/Mask_(computing)#Common_bitmask_functions
 // Eg: (READ|WRITE) & READ !== 0 then permissionBits "set" has the READ bit enabled
-export function checkPermission(permissionBits: number, permission: keyof typeof Permissions): boolean {
+export function checkPermission(permissionBits: number, permission: AllPermissions): boolean {
   return (permissionBits & Permissions[permission]) !== 0;
 }
 
-export function setPermission(permissionBits: number, permission: keyof typeof Permissions): number {
-  return permissionBits | Permissions[permission];
+export function checkMultiplePermission(permissionBits: number, permissions: AllowedPermission): PermissionRecord {
+  if (!Array.isArray(permissions)) {
+    permissions = [permissions];
+  }
+
+  const result = {
+    denied: [],
+    allowed: []
+  } as PermissionRecord;
+
+  for (const permission of permissions) {
+    if (checkPermission(permissionBits, permission)) {
+      result.allowed.push(permission);
+    } else {
+      result.denied.push(permission);
+    }
+  }
+  return result;
 }
 
-export function removePermission(permissionBits: number, permission: keyof typeof Permissions): number {
-  return permissionBits ^ Permissions[permission];
+export function setPermission(permissionBits: number, permissions: AllowedPermission): number {
+  if (!Array.isArray(permissions)) {
+    permissions = [permissions];
+  }
+  let updatedPermissionBits = permissionBits;
+  for (const permission of permissions) {
+    updatedPermissionBits |= Permissions[permission];
+  }
+  return updatedPermissionBits;
 }
 
-
+export function removePermission(permissionBits: number, permissions: AllowedPermission): number {
+  if (!Array.isArray(permissions)) {
+    permissions = [permissions];
+  }
+  let updatedPermissionBits = permissionBits;
+  for (const permission of permissions) {
+    updatedPermissionBits ^= Permissions[permission];
+  }
+  return updatedPermissionBits;
+}
