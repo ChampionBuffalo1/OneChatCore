8b4067e8e03459594d0b8fd6353c4ddf2824272f
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-02-01 19:11:41 +0530

    Added delete and patch route

--- src/api/controller/userController.ts
+++ src/api/controller/userController.ts
@@ -122,4 +122,87 @@ async function getSelf(req: Request, res: Response) {
   }
 }
 
-export { loginUser, signupUser, getSelf };
+type updateKeys = 'avatarUrl' | 'passwordHash' | 'username';
+async function userEdit(req: Request, res: Response): Promise<void> {
+  const keys = Object.keys(req.body);
+  try {
+    await prisma.user.findFirstOrThrow({
+      where: {
+        id: req.payload.userId
+      }
+    });
+    const updatedUser: Partial<Record<updateKeys, string>> = {};
+    for (const key of keys) {
+      switch (key) {
+        case 'avatarUrl': {
+          updatedUser['avatarUrl'] = req.body.avatarUrl;
+          break;
+        }
+        case 'username': {
+          const username: string = req.body.username;
+          const otherUser = await prisma.user.count({ where: { username } });
+          if (otherUser !== 0) {
+            res.status(400).json(
+              errorResponse({
+                param: 'username',
+                code: 'RESOURCE_EXISTS',
+                message: 'Username already taken.'
+              })
+            );
+            return;
+          } else {
+            updatedUser['username'] = username;
+          }
+          break;
+        }
+        case 'password': {
+          const passwordHash = await bcrypt.hash(req.body.password, bcryptSaltRounds);
+          updatedUser['passwordHash'] = passwordHash;
+          break;
+        }
+      }
+    }
+
+    const data = await prisma.user.update({
+      where: {
+        id: req.payload.userId
+      },
+      data: updatedUser,
+      select: responseStruct
+    });
+    res.status(200).json(successResponse(data));
+  } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2025') {
+      return;
+    }
+    Logger.error(err);
+    res.status(500).json(
+      errorResponse({
+        code: 'SERVICE_ERROR',
+        message: 'Internal Service Error'
+      })
+    );
+  }
+}
+
+async function deleteUser(req: Request, res: Response): Promise<void> {
+  try {
+    const user = await prisma.user.delete({
+      where: {
+        id: req.payload.userId
+      },
+      select: responseStruct
+    });
+    res.status(200).json(successResponse(user));
+  } catch (err) {
+    Logger.error(err);
+    res.status(500).json(
+      errorResponse({
+        code: 'SERVICE_ERROR',
+        message: 'Internal Service Error'
+      })
+    );
+  }
+}
+
+export { loginUser, signupUser, getSelf, userEdit, deleteUser };
--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { isAuth } from '../middlewares';
-import { getSelf } from '../controller/userController';
+import { isAuth, validateSchema } from '../middlewares';
+import { userPatch } from '../../lib/validators/userSchema';
+import { deleteUser, getSelf, userEdit } from '../controller/userController';
 
 const userRouter = Router();
 userRouter.use(isAuth);
 
 userRouter.get('/me', getSelf);
+userRouter.patch('/', validateSchema(userPatch), userEdit);
+userRouter.delete('/', deleteUser);
 
 export default userRouter;
