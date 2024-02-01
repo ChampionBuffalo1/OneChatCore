4dbe9fe233a64cdcbaf285f36f35ebb52b2f9f2a
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-02-01 19:11:09 +0530

    Added user edit schema

---
+++ src/lib/validators/userSchema.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+import { maxPassLen } from '../../Constants';
+
+const userPatch = z.object({
+  avatarUrl: z.string().url('Invalid URL').optional(),
+  username: z.string().min(1, 'Username must be at least 1 character.').optional(),
+  password: z
+    .string()
+    .min(8, 'Password must be at least 8 characters.')
+    .max(maxPassLen, `Password should not exceed ${maxPassLen} character limit`)
+    .optional()
+});
+
+export { userPatch };
