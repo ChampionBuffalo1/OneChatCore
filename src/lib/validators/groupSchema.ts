02dd9f3c35cecce110c17d2506fb3c99d988380f
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-02-03 18:34:38 +0530

    Added additional schema

--- src/lib/validators/groupSchema.ts
+++ src/lib/validators/groupSchema.ts
@@ -1,13 +1,29 @@
 import { z } from 'zod';
 
-const groupNameSchema = z.object({
+const groupCreateSchema = z.object({
   name: z
     .string({
       required_error: 'Name field missing'
     })
-    .max(64, 'name should not exceed 64 character limit')
+    .max(64, 'name should not exceed 64 character limit'),
+  description: z.string().optional()
 });
 
-const editGroupSchema = groupNameSchema;
+// min(1) is added to optional property so that if those keys are provided then the value cant be empty string
+const editGroupSchema = z.object({
+  name: z.string().min(1).max(64, 'group name should not exceed 64 character limit').optional(),
+  description: z.string().optional()
+});
 
-export { editGroupSchema, groupNameSchema };
+const inviteSchema = z.object({
+  groupId: z.string({
+    required_error: 'Group ID missing'
+  }),
+  expiresAt: z.coerce
+    .date()
+    .refine(expiresAt => expiresAt > new Date())
+    .optional(),
+  limit: z.number().min(1, 'Limit cannot be less than 1').optional()
+});
+
+export { editGroupSchema, groupCreateSchema, inviteSchema };
