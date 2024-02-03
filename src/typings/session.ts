00f8ac4cccb224a9d84864a27028b2b6705edc48
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-02-03 18:35:04 +0530

    Adding `socketPayload` to request for socket broadcast

--- src/typings/session.ts
+++ src/typings/session.ts
@@ -2,10 +2,16 @@ export interface JwtPayload {
   userId: string;
 }
 
+interface SocketPayload {
+  d: Record<string, unknown>;
+  op: string;
+}
+
 declare global {
   namespace Express {
     interface Request {
       payload: JwtPayload;
+      socketPayload: SocketPayload;
     }
   }
 }
