7d62127e2b321969d952239e0740c5d4e171df20
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-02-20 20:13:05 +0530

    falling back to global middleware handlers if no payload

--- src/websocket/handleBroadcasting.ts
+++ src/websocket/handleBroadcasting.ts
@@ -1,5 +1,8 @@
-import type { Request } from 'express';
+import type { Request, NextFunction } from 'express';
 
-export default function handleBroadcasting(req: Request, _: unknown) {
+export default function handleBroadcasting(req: Request, _: unknown, next: NextFunction) {
+  if (!req.socketPayload) {
+    next();
+  }
   console.log(req.socketPayload);
 }
