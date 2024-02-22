838506c069488e3a28b679a5f06c12ff6dbb26b5
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-02-22 18:50:49 +0530

    Broadcasting group events to each active member

--- src/websocket/handleBroadcasting.ts
+++ src/websocket/handleBroadcasting.ts
@@ -1,8 +1,15 @@
+import store from './Store';
 import type { Request, NextFunction } from 'express';
 
 export default function handleBroadcasting(req: Request, _: unknown, next: NextFunction) {
   if (!req.socketPayload) {
     next();
+    return;
   }
-  console.log(req.socketPayload);
+  const members = store.getGroupConnections(req.socketPayload.d.group.id);
+  if (!members) return;
+  for (const socketKey of members) {
+    const socket = store.getConnection(socketKey);
+    socket?.send(JSON.stringify(req.socketPayload.d));
+  }
 }
