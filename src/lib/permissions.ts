ebbc5adecf633228327a173f1cf3735ca3b5bafd
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-03-21 23:51:17 +0530

    fix: Fixed permissions with bit operations

--- src/lib/permissions.ts
+++ src/lib/permissions.ts
@@ -3,14 +3,13 @@
 const Permissions = {
   READ_MESSAGES: 1 << 0, // 000 00 001
   WRITE_MESSAGES: 1 << 1,
-  MANAGE_MESSAGES: (1 << 2) | (1 << 1) | (1 << 0),
+  MANAGE_MESSAGES: (1 << 2) - 1,
 
   CHANGE_PERMISSION: 1 << 3, // 1
 
   INVITE_MEMBER: 1 << 4,
-  KICK_MEMBER: 1 << 5,
-  MANAGE_GROUP: 1 << 6, // modify group name, desc etc
-  ADMINISTRATOR: (1 << 7) - 1 // 111 1 111
+  MANAGE_GROUP: 1 << 5, // modify group name, desc etc
+  ADMINISTRATOR: (1 << 6) - 1 // 111 1 111
 };
 
 type AllPermissions = keyof typeof Permissions;
@@ -23,7 +22,7 @@ type PermissionRecord = {
 // Reference: https://en.wikipedia.org/wiki/Mask_(computing)#Common_bitmask_functions
 // Eg: (READ|WRITE) & READ !== 0 then permissionBits "set" has the READ bit enabled
 export function checkPermission(permissionBits: number, permission: AllPermissions): boolean {
-  return (permissionBits & Permissions[permission]) !== 0;
+  return (permissionBits & Permissions[permission]) === Permissions[permission];
 }
 
 export function checkMultiplePermission(permissionBits: number, permissions: AllowedPermission): PermissionRecord {
