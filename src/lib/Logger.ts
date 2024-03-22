e9f599b320d48378932d072735ba258b0ba27c5e
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-03-22 09:55:19 +0530

    fix: Only adding file transport if grafana was not setup

--- src/lib/Logger.ts
+++ src/lib/Logger.ts
@@ -2,15 +2,7 @@ import winston from 'winston';
 import { isProd } from '../Constants';
 import LokiTransport from 'winston-loki';
 
-const transports: winston.transport[] = [
-  new winston.transports.Console(),
-  new winston.transports.File({ filename: 'combined.log', dirname: 'logs' }),
-  new winston.transports.File({
-    filename: 'error.log',
-    level: 'error',
-    dirname: 'logs'
-  })
-];
+const transports: winston.transport[] = [new winston.transports.Console()];
 let format = winston.format.combine(
   winston.format.splat(),
   winston.format.timestamp({ format: 'HH:mm:ss DD-MM-YYYY' }),
@@ -20,21 +12,28 @@ let format = winston.format.combine(
 if (isProd) {
   // Removing console logs in prod
   transports.shift();
-  if (process.env.GRAFANA_IP) {
-    // Removing disk logs if grafana is setup in prod
-    transports.pop();
-    transports.pop();
-    const loki = new LokiTransport({
-      host: process.env.GRAFANA_IP,
-      onConnectionError: console.error, // Log error to console if connection problem
-      labels: { job: 'onechat-logs' },
-      basicAuth: process.env.LOKI_AUTH
-    });
-    transports.push(loki);
-    format = winston.format.json();
-  }
 }
 
+if (process.env.GRAFANA_IP) {
+  const loki = new LokiTransport({
+    host: process.env.GRAFANA_IP,
+    onConnectionError: console.error, // Log error to console if connection problem
+    labels: { job: 'onechat-logs' },
+    basicAuth: process.env.LOKI_AUTH
+  });
+  transports.push(loki);
+  format = winston.format.json();
+} else {
+  // Adding disk logs if grafana loki isn't setup
+  transports.push(new winston.transports.File({ filename: 'combined.log', dirname: 'logs' }));
+  transports.push(
+    new winston.transports.File({
+      filename: 'error.log',
+      level: 'error',
+      dirname: 'logs'
+    })
+  );
+}
 const Logger = winston.createLogger({
   level: 'debug',
   format,
